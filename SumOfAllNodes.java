ab9cda0d3ee1bd6c161056eca0c25a5547958407
rhtrai2 <rht.rai2@gmail.com>
2020-04-01 02:12:23 +0530

    Sum of all nodes in a binary tree

--- SumOfAllNodes.java
+++ SumOfAllNodes.java
@@ -1,3 +1,35 @@
 public class SumOfAllNodes {
-    
+
+    Node root;
+
+    int allNodeSum(Node root) {
+        if(root == null) {
+            return 0;
+        }
+        return root.data + allNodeSum(root.left) + allNodeSum(root.right);
+    }
+
+    public static void main(String[] args) {
+        SumOfAllNodes tree = new SumOfAllNodes();
+        tree.root= new Node(1);
+        tree.root.left= new Node(2);
+        tree.root.right= new Node(3);
+        tree.root.left.left= new Node(4);
+        tree.root.left.right= new Node(5);
+
+        System.out.println("Sum of all Nodes of Binary tree:" + tree.allNodeSum(tree.root));
+
+    }
+
+}
+
+class Node {
+
+    int data;
+    Node left, right;
+
+    Node(int key) {
+        data = key;
+        left = right = null;
+    }
 }
