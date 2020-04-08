87aaa8666987e7f6a8016ae9b0d7b18741dc719b
rhtrai2 <rht.rai2@gmail.com>
2020-04-09 01:44:03 +0530

    ITERATIVE INORDER TRAVERSAL

--- ItreativeInorderTraversal.java
+++ ItreativeInorderTraversal.java
@@ -1,2 +1,45 @@
+import java.util.Stack;
+
 public class ItreativeInorderTraversal {
+
+    Node root;
+
+    void  iterativeInorder(Node node) {
+        if(node == null)
+            return;
+
+        Node current = node;
+        Stack<Node> s = new Stack<Node>();
+//        s.push(current);
+        while(s.size() > 0 || current != null) {
+            while(current != null) {
+                s.push(current);
+                current = current.left;
+            }
+
+            current = s.pop();
+            System.out.print(" "+ current.data);
+
+            current = current.right;
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        ItreativeInorderTraversal tree = new ItreativeInorderTraversal();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+//        tree.root.right.left = new Node(2);
+        tree.root.right.right = new Node(6);
+
+        int x = 2;
+
+        System.out.println("ITERATIVE INORDER:" );
+        tree.iterativeInorder(tree.root);
+    }
+
 }
